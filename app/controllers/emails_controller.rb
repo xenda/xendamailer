a4b91ea84eb422aa3be34b759615b977def7a99c
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-04-02 11:38:00 -0500

    Added Campfire support

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -31,6 +31,29 @@ class EmailsController < ApplicationController
     
     #We inform the proper parties
     IssueMailer.issue_receive(sender, subject, code, pivotal_issue_url).deliver
+    
+    campfire = Tinder::Campfire.new 'xenda', :token => '1769bcfa6c5fd00ff96749c1325a2d8491aad6f1'
+    room = campfire.find_room_by_name 'General'
+
+    messages = ["No quiero entromenterme, pero", 
+                "¿Molesto? Espero que no ya que", 
+                "¡Hola Avengers! Les comento que",
+                "OH POR DIOS VAMOS A MORIR...",
+                "¡Guarden a las mujeres y niños! ",
+                "¿Todo bien por acá? ¿No tienen hambre? Creo que hay una cafetería en frente muy buena. Ah, por cierto, ",]
+    exit_messages = [ "¡El primero en resolverla se gana un chocolate!",
+                      "Y eso es todo. Que sigan teniendo un buen día. Jarvis off.",
+                      "Bueno, tal vez no sea tan grave, pero mírenlo :)",
+                      "Listo... creo que no queda más que decir. ¡A por ello!",
+                      "¿La van a atender ahora? Me avisan cualquier cosa",
+                      "And that's all folks!"]
+                      
+    random_header = messages.shuffle.first
+    random_exit = exit_messages.shuffle.first
+    
+    room.speak "#{random_header} se ha registrado la incidencia #{code} de #{sender}"
+    room.speak "Nos dicen ésto: #{subject}"
+    room.speak random_exit
 
     head :ok
     
